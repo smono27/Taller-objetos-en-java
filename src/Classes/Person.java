6a5b4a038f2607490d51c0a506836e6579e4f63a
Simon <simsote27@gmail.com>
2023-07-02 14:36:19 -0500

    Add person java class

---
+++ src/Classes/Person.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+import java.util.Date;
+
+/**
+ *
+ * @author saymo
+ */
+public class Person {
+    public String name;
+    public String lastName1;
+    public String lastName2;
+    public Date dateBirth;
+    public float height;
+
+    public Person() {
+    }
+    
+    public Person(String name, String lastName1, String lastName2, Date dateBirth, float height) {
+        this.name = name;
+        this.lastName1 = lastName1;
+        this.lastName2 = lastName2;
+        this.dateBirth = dateBirth;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
